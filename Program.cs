bac751286e0cfb9e3cc385a5356a6e15c3f6328a
bokiex <kohbokyan@gmail.com>
2021-08-11 23:37:51 +0800

    Added static commands

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
@@ -28,35 +26,38 @@ namespace bot_yan {
 
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
-
             botClient.StopReceiving();
-        }
-
-        static async Task<Catfact> GetFactAsync(string path){
-            Catfact catfact = null;
-            HttpResponseMessage response = await client.GetAsync(path);
-            if (response.IsSuccessStatusCode)
-            {
-                catfact = await response.Content.ReadAsAsync<Catfact>();
-            }
-            return catfact;
         }
 
         static async void Bot_OnMessage(object sender, MessageEventArgs e) {
             Catfact catfact = new Catfact();
             var url = "https://cat-fact.herokuapp.com/facts";
             Console.WriteLine($"Created at {url}");
-            catfact = await GetFactAsync(url);
-            Console.WriteLine(catfact.text);
             if (e.Message.Text != null)
             {
                 Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
-
+                var text = e.Message.Text;
+                var reply = "";
+                if (text == "/joke"){
+                    reply = "this is a joke";
+                }
+                else{
+                    reply = "hello";
+                }
                 await botClient.SendTextMessageAsync(
                 chatId: e.Message.Chat,
-                text:   "You said:\n" + e.Message.Text
-                );
+                text: reply
+                );  
             }
         }
+
+        // static async Task<Catfact> GetCatfactAsync(string path){
+        //     var catfact = "";
+        //     HttpResponseMessage response = await client.GetAsync(path);
+        //     if(response.IsSuccessStatusCode){
+        //         catfact = await response.Content.ReadAsStringAsync();
+        //     }
+        //     return Console.WriteLine(catfact);
+        // }
     }
 }
